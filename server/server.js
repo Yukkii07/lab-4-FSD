8dcd2469ef8c7a95b9020b8df09fa52d4da162bc
Yukkii07 <yukeshp627@gmail.com>
2026-02-19 10:16:53 +0530

    -
     nm

---
+++ server/server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello! Your Express server is running 🚀');
+});
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
